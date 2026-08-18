aedd648b13348e2aed12db8baffb6e5f22ecadfd
Stylo321 <silolwanda@gmail.com>
2026-08-18 18:14:54 +0200

    Push CoffeeAndChill workspace code

---
+++ TestStorageConnection.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace CoffeeNChill.Functions
+{
+    public class TestStorageConnection
+    {
+        [Function("TestStorageConnection")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "test-storage")]
+            HttpRequest req)
+        {
+            //this line here basically finds the connection string in the local.settings.json file and uses it to connect to the Azure Table Storage
+            //answers the "what storage connection has my application been configured to use?"
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            //creates the client that will be used to connect to the Azure Table Storage
+            TableServiceClient serviceClient =
+                new TableServiceClient(connectionString);
+
+            string tableName = "TestTable";
+
+            //basically sais create table if it doesn't exist, if it does exist, do nothing 
+
+            await serviceClient.CreateTableIfNotExistsAsync(tableName);
+             
+            return new OkObjectResult(new
+            {
+                message = "Successfully connected to Azure Table Storage!",
+                table = tableName
+            });
+        }
+    }
+}
